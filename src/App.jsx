dac16fa5e277e38ffe72013d175f0070f2601938
abhinavpant <141048769+abhinavpant@users.noreply.github.com>
2024-05-17 16:02:16 +0530

    TopProducts,Banner,Testimonials completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Hero from "./Components/Hero/Hero"
 import Products from "./Components/Products/Products"
 import AOS from "aos";
 import "aos/dist/aos.css"
+import TopProducts from "./Components/TopProducts/TopProducts";
+import Banner from "./Components/Banner/Banner";
+import Subscribe from "./Components/Subscribe/Subscribe";
+import Testimonials from "./Components/Testimonials/Testimonials";
 const App= ()=> {
 
   React.useEffect(() => {
@@ -20,6 +24,11 @@ const App= ()=> {
       <Navbar />
       <Hero/>
       <Products/>
+      <TopProducts/>
+      <Banner/>
+      <Subscribe/>
+      <Products/>
+      <Testimonials/>
     </div>
   )
 }
